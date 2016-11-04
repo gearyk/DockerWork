f309e739a59c108fa3b87df9ac58b27a810fa1bf
gearyk <gearyk@gmail.com>
2016-11-04 15:57:41 +0000

    Added some PageFactory work

--- DockerWork/src/main/java/com/cit/ie/pageobjects/LoginPagePO.java
+++ DockerWork/src/main/java/com/cit/ie/pageobjects/LoginPagePO.java
@@ -3,6 +3,7 @@ package com.cit.ie.pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -30,26 +31,47 @@ public class LoginPagePO extends HelperMethods
 	public static final String USERNAME_FIELD_XPATH = ".//input[@name='uName']";
 	public static final String PASSWORD_FIELD_XPATH = ".//input[@name='pWrd']";
 	public static final String LOGIN_BUTTON_XPATH = ".//*/button[@aria-label='Login']";
+	public static final String VERSION_NUMBER_XPATH = ".//*/div[text()[contains(.,'Version')]]";
+	public static final String LOGIN_LOGO_XPATH = ".//*[@id='login_logo']";
 
-	public WebElement loginPageTitle(){
-		WebElement element = (new WebDriverWait(getDriver(), 10)).until(ExpectedConditions.elementToBeClickable(By.xpath(LOGIN_PAGE_TITLE_XPATH)));
-		return element;
-	}
 	
-	public WebElement usernameField(){
-		WebElement element = (new WebDriverWait(getDriver(), 10)).until(ExpectedConditions.elementToBeClickable(By.xpath(USERNAME_FIELD_XPATH)));
-		return element;
-	}
+	@FindBy(xpath=LOGIN_PAGE_TITLE_XPATH)
+	public WebElement loginPageTitle;
 	
-	public WebElement passwordField(){
-		WebElement element = (new WebDriverWait(getDriver(), 10)).until(ExpectedConditions.elementToBeClickable(By.xpath(PASSWORD_FIELD_XPATH)));
-		return element;
-	}
+	@FindBy(xpath=USERNAME_FIELD_XPATH)
+	public WebElement usernameField;
 	
-	public WebElement loginButton(){
-		WebElement element = (new WebDriverWait(getDriver(), 10)).until(ExpectedConditions.elementToBeClickable(By.xpath(LOGIN_BUTTON_XPATH)));
-		return element;
-	}
+	@FindBy(xpath=PASSWORD_FIELD_XPATH)
+	public WebElement passwordField;
+	
+	@FindBy(xpath=LOGIN_BUTTON_XPATH)
+	public WebElement loginButton;
+	
+	@FindBy(xpath=LOGIN_LOGO_XPATH)
+	public WebElement loginLogo;
+	
+	@FindBy(xpath=VERSION_NUMBER_XPATH)
+	public WebElement versionNumber;
+	
+//	public WebElement loginPageTitle(){
+//		WebElement element = (new WebDriverWait(getDriver(), 10)).until(ExpectedConditions.elementToBeClickable(By.xpath(LOGIN_PAGE_TITLE_XPATH)));
+//		return element;
+//	}
+//	
+//	public WebElement usernameField(){
+//		WebElement element = (new WebDriverWait(getDriver(), 10)).until(ExpectedConditions.elementToBeClickable(By.xpath(USERNAME_FIELD_XPATH)));
+//		return element;
+//	}
+//	
+//	public WebElement passwordField(){
+//		WebElement element = (new WebDriverWait(getDriver(), 10)).until(ExpectedConditions.elementToBeClickable(By.xpath(PASSWORD_FIELD_XPATH)));
+//		return element;
+//	}
+//	
+//	public WebElement loginButton(){
+//		WebElement element = (new WebDriverWait(getDriver(), 10)).until(ExpectedConditions.elementToBeClickable(By.xpath(LOGIN_BUTTON_XPATH)));
+//		return element;
+//	}
 	
 	
 	
--- DockerWork/src/main/java/com/cit/ie/selenium/WebDriverManager.java
+++ DockerWork/src/main/java/com/cit/ie/selenium/WebDriverManager.java
@@ -90,14 +90,16 @@ public class WebDriverManager {
         String HubIP=ce.getAddressOfRemoteServer().getHost();
         int HubPort=ce.getAddressOfRemoteServer().getPort();
         HttpHost host = new HttpHost(ce.getAddressOfRemoteServer().getHost(), HubPort);
-        DefaultHttpClient client = new DefaultHttpClient();
+        @SuppressWarnings("deprecation")
+		DefaultHttpClient client = new DefaultHttpClient();
         URL testSessionApi = new URL("http://" + HubIP + ":" + HubPort + "/grid/api/testsession?session=" +  driver.getSessionId());
         BasicHttpEntityEnclosingRequest r = new  BasicHttpEntityEnclosingRequest("POST", testSessionApi.toExternalForm());
         HttpResponse response  = client.execute(host,r);
         System.out.println(response);
         System.out.println(response.getEntity());
         JSONObject object = new JSONObject(EntityUtils.toString(response.getEntity()));
-        String proxyID =(String) object.get("proxyId");
+        @SuppressWarnings("unused")
+		String proxyID =(String) object.get("proxyId");
 
 	    }
 	 
